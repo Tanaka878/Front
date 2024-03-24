0d2b63f3cabc741a66a1c8698bb317ea228ca1ff
Tanaka878 <musungaretanaka@gmail.com>
2024-03-24 14:45:49 +0200

    adding input validations

--- src/LoginPage/NewUser.js
+++ src/LoginPage/NewUser.js
@@ -1,30 +1,86 @@
 import React from 'react'
 
 const NewUser = () => {
+
+  const[User, changeUser] = React.useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    pin:"",
+    confirmPin:"",
+    date:""
+
+  })
+
+  function handleChange(event){
+    const{name, value, checked,type} = event.target
+
+    changeUser((prev)=>{
+      return {
+        ...prev,
+            [name] : type==='checkbox' ? checked: value
+
+      };
+
+    })
+
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    if(User.pin !== User.confirmPin){
+      console.log("The passwords do not match");
+
+
+    }else{
+      console.log("Submitted Information");
+    }
+
+  }
+
+  console.log(User)
+
+
   return (
-    <form className='newUser'>
+    <form className='newUser' onSubmit={handleSubmit}>
       
       <div className='form-group'>
         <label htmlFor='name'>First Name :</label>
-        <input name='firstName' required/>
+        <input name='firstName' required onChange={handleChange}/>
       </div>
 
         <div className='form-group'>
           <label htmlFor='lastName'>Last Name :</label>
-          <input name='lastName' required/>
+          <input name='lastName' required onChange={handleChange}/>
         </div>
 
        <div className='form-group'>
          <label htmlFor='date'>DOB :</label>
-         <input type='date' name='date'/>
+         <input type='date' name='date' onChange={handleChange}/>
+       </div>
+
+       <div className='email'>
+        <label className='form-group' htmlFor='email'>Email :</label>
+        <input type='email' required onChange={handleChange} name='email'/>
+
+        <label htmlFor='newsletter'>Subscribe for monthly Newsletter</label>
+        <input type='checkbox' name='newsletter'/>
        </div>
 
        <div>
-        <label className='form-group' htmlFor='email'>Email :</label>
-        <input type='email' required/>
-
-        <label htmlFor='newsletter'>Subscribe for monthly Newsletter</label>
+        <h2>Enter New Pin</h2>
+        <label>New Pin</label>
+        <input type='password'required onChange={handleChange} name='pin'/>
        </div>
+
+       <div className='form-group'>
+        <label>Confirm Pin :</label>
+        <input type='password' required onChange={handleChange} name='confirmPin'/>
+       </div>
+
+       <button className='form-button'>Create Account</button>
+
 
         
       
